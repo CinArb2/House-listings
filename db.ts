28e6a8d66f6cff3e4bc102061cf457582ab5ecdd
CinArb2 <cindypao27@gmail.com>
2023-06-28 08:05:07 -0500

    retrieve data from db and adding latest house listing

--- db.ts
+++ db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"
 
-
+// Define the Sequelize instance  that represents the database connection
 const db = new Sequelize('house-listings-sm','', '', {
   storage: './database.sqlite',
   dialect: 'sqlite',
--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-import express, {Application, Request, Response} from 'express'
-import {filterHouses} from './filterData'
+import express, { Application, Request, Response } from 'express'
 import db from './db'
-import { v4 as uuidv4 } from 'uuid'
 import { HouseListingInstance } from './Model'
-import { FrHouseHit, HouseListing } from './types/house-listing'
+import { createDbdata, filterHouses } from './filterData'
+import { FrHouseHit } from './types/house-listing'
 
 db.sync().then(() => {
   console.log('connect to Database')
@@ -11,46 +10,33 @@ db.sync().then(() => {
 
 const app: Application = express()
 
-/*
- * TODO: first I need to validate if db is already created and then check that
- * there are not multiples fr_property_id in db
-
 app.get('/api/listings/refresh', async (_req: Request, res: Response) => {
-  try{
-    const data = await filterHouses()
+  const latestHouseListing: FrHouseHit[] = await filterHouses()
+  const houseListingDb = await HouseListingInstance.findAll()
 
-    const houseListings = data.map((house: FrHouseHit) => {
-      const id = uuidv4()
-      const listingDetails = house._source.listing
+  const missingHouseListings: FrHouseHit[] = latestHouseListing.filter(({ _source }) => {
+    const frPropertyId = _source.listing.fr_property_id;
+    return !houseListingDb.some((houseDb) => houseDb.getDataValue('fr_property_id') === frPropertyId);
+  })
 
-      return {
-        id,
-        area: listingDetails.area,
-        price: listingDetails.price,
-        fr_property_id: listingDetails.fr_property_id,
-        agency_name: listingDetails.client.company_name,
-        owner_name: listingDetails.client.first_name,
-        owner_last_name: listingDetails.client.last_name,
-        date_published: listingDetails.dates.published,
-        contact_email: listingDetails.contact.emails?.[0].email ?? '',
-        contact_phone: listingDetails.contact.phones?.[0].phone_number ?? '',
-        property_type: listingDetails.property_type[0].name,
-        location_commune: listingDetails.locations.communes?.[0].name ?? '',
-        locations_neighbourhood: listingDetails.locations.neighbourhoods?.[0].name ?? ''
-      }
-    })
-    const record = await HouseListingInstance.bulkCreate(houseListings)
-    return res.json({ record, msg: "Successfully created" })
-  }catch (e) {
-    return res.json({ msg: "fail to create", status: 500, error: e });
+  let record: HouseListingInstance[] = []
+
+  if(missingHouseListings.length > 0) {
+    const dbData = createDbdata(missingHouseListings)
+    record = await HouseListingInstance.bulkCreate(dbData)
   }
-})
-*/
 
-app.get('/api/listings', (_req: Request, res: Response): void => {
-  res.send('get list of houses!!')
+  res.status(200).json({
+    record,
+  })
 })
 
+app.get('/api/listings', async (_req: Request, res: Response) => {
+  const houseList = await HouseListingInstance.findAll()
+  res.status(200).json({
+    houseList,
+  })
+})
 
 const PORT = process.env.PORT || 5000
 
