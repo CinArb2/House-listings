9b7e2c7deec84cdd77a3fb1409b8f5393329432f
CinArb2 <cindypao27@gmail.com>
2023-06-28 08:03:55 -0500

    add createdbData logic

--- filterData.ts
+++ filterData.ts
@@ -3,6 +3,8 @@ import axios from 'axios'
 import * as body from './body-request.json'
 import _ from 'lodash'
 import moment from 'moment'
+import { FrHouseHit, FrHouseListing } from 'types/house-listing'
+import { v4 as uuidv4 } from 'uuid'
 
 const getAllHouseListings = async () => {
   try {
@@ -30,4 +32,29 @@ export const filterHouses = async () => {
   } catch (error) {
     console.error(error)
   }
+}
+
+export const createDbdata = (data: FrHouseHit[]) => {
+  const houseListings = data.map((house: FrHouseHit) => {
+    const id = uuidv4()
+    const listingDetails: FrHouseListing = house._source.listing
+
+    return {
+      id,
+      area: listingDetails.area,
+      price: listingDetails.price,
+      fr_property_id: listingDetails.fr_property_id,
+      agency_name: listingDetails.client.company_name,
+      owner_name: listingDetails.client.first_name,
+      owner_last_name: listingDetails.client.last_name,
+      date_published: listingDetails.dates.published,
+      contact_email: listingDetails.contact.emails?.[0].email ?? '',
+      contact_phone: listingDetails.contact.phones?.[0].phone_number ?? '',
+      property_type: listingDetails.property_type[0].name,
+      location_commune: listingDetails.locations?.communes?.[0].name ?? '',
+      locations_neighbourhood: listingDetails.locations?.neighbourhoods?.[0].name ?? ''
+    }
+  })
+
+  return houseListings
 }
